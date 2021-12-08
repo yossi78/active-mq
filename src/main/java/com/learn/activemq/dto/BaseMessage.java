e2ce614aa9a82ff69b2a91ce457e4aa96c80b1b5
yossi78 <yossi78@gmail.com>
2021-12-08 08:46:28 +0200

    Implement serilizable

--- src/main/java/com/learn/activemq/dto/BaseMessage.java
+++ src/main/java/com/learn/activemq/dto/BaseMessage.java
@@ -1,20 +1,16 @@
 package com.learn.activemq.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
 import java.io.Serializable;
 
 
+
 @Data
+@AllArgsConstructor
 public abstract class BaseMessage implements Serializable {
 
     protected String source;
     protected String message;
 
-
-    public BaseMessage(String source,String message){
-        this.source=source;
-        this.message=message;
-    }
 
 }
--- src/main/java/com/learn/activemq/dto/NotificationMessage.java
+++ src/main/java/com/learn/activemq/dto/NotificationMessage.java
@@ -1,24 +1,15 @@
 package com.learn.activemq.dto;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.io.Serializable;
 
 
 
-public class NotificationMessage extends BaseMessage implements Serializable{
 
 
+public class NotificationMessage extends BaseMessage {
 
     public NotificationMessage(String source,String message){
         super(source, message);
     }
 
-//    @Override
-//    public String toString() {
-//        return "{notification_message__source="+source + "___message="+message+"}";
-//    }
 
 
 }
