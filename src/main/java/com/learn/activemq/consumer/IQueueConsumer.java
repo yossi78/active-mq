537074a88f1d7acd428959d0810882a0f9c63892
yossi78 <yossi78@gmail.com>
2021-12-20 08:22:26 +0200

    first

--- src/main/java/com/learn/activemq/consumer/IQueueConsumer.java
+++ src/main/java/com/learn/activemq/consumer/IQueueConsumer.java
@@ -2,6 +2,7 @@ package com.learn.activemq.consumer;
 import com.learn.activemq.dto.NotificationMessage;
 
 
+
 public interface IQueueConsumer {
 
     public void messageListener(NotificationMessage message);
