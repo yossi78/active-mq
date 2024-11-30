f7422290b99f5181513f11a35f1e07da7170c9e0
yossi78 <yossi78@gmail.com>
2024-11-30 20:50:39 +0200

    add postman

--- src/main/java/com/example/active_mq/api/UserController.java
+++ src/main/java/com/example/active_mq/api/UserController.java
@@ -4,6 +4,7 @@ import com.example.active_mq.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
